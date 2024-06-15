a8c397d552bf946935b0969eed6b9d46d30c6e37
Trần Tấn Dũng <dung.trantan01@gmail.com>
2024-06-15 16:50:13 +0700

    cmt

--- Assets/_Submodules/photon-bomber-io-core/UI/UIRegionMainMenu.cs
+++ Assets/_Submodules/photon-bomber-io-core/UI/UIRegionMainMenu.cs
@@ -4,14 +4,17 @@ using UnityEngine.UI;
 public class UIRegionMainMenu : MonoBehaviour
 {
     [SerializeField] private Text textRegion;
+    [SerializeField] private GameObject matchMakingObj;
 
     private void OnEnable()
     {
         if (SimplePhotonNetworkManager.Singleton.isConnectOffline)
         {
+            matchMakingObj.SetActive(false);
             textRegion.text = "Offline";
             return;
         }
+        matchMakingObj.SetActive(true);
         var uiPhotonNetworking = GetComponentInParent<UIPhotonNetworking>();
         string regionCode = PlayerPrefs.GetString("SAVE_SELECTED_REGION", string.Empty);
         foreach (var region in uiPhotonNetworking.selectableRegions)
